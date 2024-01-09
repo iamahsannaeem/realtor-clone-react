2b18e29d177b3f4eb4de4cb4b0ca992e196878b1
Muhammad Ahsan Naeem <ahsannaeem480@gmail.com>
2024-01-09 12:20:42 +0500

    Update Packages React Router/ React Router Dom, Created pages and set their routes in App Main page component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignOut from "./Pages/SignOut";
+import Offers from "./Pages/Offers";
+import Forgotpassword from "./Pages/Forgotpassword";
+import Profile from "./Pages/Profile";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/forgot-password" element={<Forgotpassword />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
